89a04483ca891ec6c6f5361be46024291545ca30
mirkettinho <mirkodifranco30@gmail.com>
2023-04-03 16:24:50 +0200

    [add] array

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+
+// ARREY OGGETTI
+const gruppo = [
+  {
+    nome: "Anthony Joshua",
+    ruolo: "Pugile",
+    Foto: "ciao"
+  },
+  {
+    nome: "Franchino Er Criminale",
+    ruolo: "Content Creator",
+    Foto: "ciao"
+  },
+  {
+    nome: "Cristiano",
+    ruolo: "Ronaldo",
+    Foto: "ciao"
+  },
+  {
+    nome: "Salvatore Aranzulla",
+    ruolo: "Blogger",
+    Foto: "ciao"
+  },
+  {
+    nome: "Arnold Schwarzenegger",
+    ruolo: "Attore, Politico, Imprenditore, Produttore Cinematografico e Culturista",
+    Foto: "ciao"
+  },
+  {
+    nome: "Will Smith",
+    ruolo: "Attore",
+    Foto: "ciao"
+  }
+]
+
+for(let membro of gruppo)
+console.log(membro)
